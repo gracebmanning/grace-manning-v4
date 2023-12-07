c29e0461a78866158709ba6a1b281290423b5227
Grace Manning <gracebmanning@gmail.com>
2023-12-07 14:10:54 -0800

    remove personal projects section

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -1,8 +1,7 @@
 import './projects.css';
 import arrow from '../../images/arrow-down.svg';
-import arrowLight from '../../images/arrow-down-light.svg';
 import { clickScroll } from '../clickScroll';
-import { projectItems, personalProjectItems, thingsICareAbout } from './projectsList';
+import { projectItems } from './projectsList';
 
 const skills = ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'React', 'Angular', 'Node.js', 'Flutter/Dart', 'Android Studio', 'Xcode', 'Figma', 'Git/GitHub', 'AWS', 'MongoDB', 'MySQL', 'Python', 'C++', 'C', 'Java', 'Racket'];
 const skillItems = skills.map((skill) =>
@@ -25,24 +24,6 @@ export default function Projects() {
                 <h2 className="h2">Projects</h2>
                 <div className="projectsContainer">
                     {projectItems}
-                </div>
-            </div>
-            <img id="arrow" src={arrow} alt="arrow pointing down" onClick={() => clickScroll('projects2')} />
-        </div>
-        <div className="section-gradient" id="projects2">
-            <div className="rectangle">
-                <h2 className="h2">Personal Projects</h2>
-                <div className="projectsContainer">
-                    {personalProjectItems}
-                </div>
-            </div>
-            <img id="arrow" src={arrowLight} alt="arrow pointing down" onClick={() => clickScroll('care-about')} />
-        </div>
-        <div className="section-solid" id="care-about">
-            <div className="container">
-                <h2 className="h2">Other Things I Care About</h2>
-                <div className="projectsContainer">
-                    {thingsICareAbout}
                 </div>
             </div>
             <img id="arrow" src={arrow} alt="arrow pointing down" onClick={() => clickScroll('contact')} />
